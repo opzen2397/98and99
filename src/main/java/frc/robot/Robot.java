6982bf494ca9bab68d0ef19994e84b7bcb389b85
opzen2397 <eden20090817@gmail.com>
2025-12-16 15:34:11 +0800

    12/16

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  public Robot() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+}
